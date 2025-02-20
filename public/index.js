d498ce2ce0c7b1ba77deb7d50569cc2c33697275
Nee-Sudo <vs0158213@gmail.com>
2025-02-20 15:24:11 +0530

    show password function added in login and signup form

--- public/index.js
+++ public/index.js
@@ -23,4 +23,26 @@
 
   console.log('index.js loaded');
 
- 
+  try{
+    const jwt = localStorage.getItem('jwt');
+    if(jwt){
+      console.log('jwt found');
+      const loginElement = document.getElementById('login');
+      const LoginElement = document.getElementById('Login');
+      document.getElementById('signup').style.display= 'none';
+      document.getElementById('Signup').style.display= 'none';
+      if (loginElement) {
+        loginElement.innerText = 'Profile';
+        LoginElement.innerText = 'Profile';
+        loginElement.setAttribute('href', '/dashboards');
+        LoginElement.setAttribute('href', '/dashboards');           
+      } else {
+        console.error('Element with ID "login" not found');
+      }
+    } else {
+      console.log('jwt not found');
+    }
+  } catch (error) {
+    console.error('Error accessing localStorage:', error);
+  }
+  
